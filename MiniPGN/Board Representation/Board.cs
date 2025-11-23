71cfc0eb5c5f0cc183d7ebc69758bce9548ab3ae
OpDragon26 <25opdragon25@gmail.com>
2025-11-23 18:10:19 +0100

    make move function

--- MiniPGN/Board Representation/Board.cs
+++ MiniPGN/Board Representation/Board.cs
@@ -2,11 +2,85 @@ using System.Runtime.CompilerServices;
 
 namespace MiniPGN.Board_Representation;
 using static Utils;
+using static Pieces;
 
 public class Board
 {
+    private int turn;
     private PieceBoard board;
+    private Bitboard bitboards;
     
+    public void MakeMove(Move move)
+    {
+        bool promotion = move.Flag == Flag.Promotion;
+        bool capture = board[move.Target] != Empty; 
+            
+        byte movingPiece = promotion ? move.Promotion : board[move.Source];
+
+        bitboards[movingPiece, move.Source] = false;
+        bitboards[movingPiece, move.Target] = true;
+        if (capture)
+            bitboards[board[move.Target], move.Target] = false;
+        
+        board[move.Source] = Empty;
+        board[move.Target] = movingPiece;
+        
+        if (promotion)
+            return;
+
+        if ((byte)move.Flag > 1)
+            HandleSpecialMove(move);
+    }
+
+    private void HandleSpecialMove(Move move)
+    {
+        switch (move.Flag)
+        {
+            case Flag.WhiteShortCastle:
+            {
+                board[7] = Empty;
+                board[5] = WRook;
+                bitboards[WRook, 7] = false;
+                bitboards[WRook, 5] = true;
+            } break;
+            
+            case Flag.WhiteLongCastle:
+            {
+                board[0] = Empty;
+                board[3] = WRook;
+                bitboards[WRook, 0] = false;
+                bitboards[WRook, 3] = true;
+            } break;
+
+            case Flag.BlackShortCastle:
+            {
+                board[63] = Empty;
+                board[61] = BRook;
+                bitboards[BRook, 63] = false;
+                bitboards[BRook, 61] = true;
+            } break;
+            
+            case Flag.BlackLongCastle:
+            {
+                board[56] = Empty;
+                board[59] = BRook;
+                bitboards[BRook, 56] = false;
+                bitboards[BRook, 59] = true;
+            } break;
+            
+            case Flag.WhiteEnPassant:
+            {
+                board[move.Target - 8] = Empty;
+                bitboards[BPawn, move.Target - 8] = false;
+            } break;
+
+            case Flag.BlackEnPassant:
+            {
+                board[move.Target + 8] = Empty;
+                bitboards[WPawn, move.Target + 8] = false;
+            } break;
+        }
+    }
     public byte this[int index]
     {
         get => board[index];
@@ -18,6 +92,11 @@ public class Board
         get => board[GetIndex(file, rank)];
         set => board[GetIndex(file, rank)] = value;
     }
+
+    public Board Clone()
+    {
+        return new Board { board = board, bitboards = bitboards , turn = turn };
+    }
 }
 
 [InlineArray(64)]
@@ -26,7 +105,7 @@ public struct PieceBoard
     public byte piece;
 }
 
-[InlineArray(14)]
+[InlineArray(15)]
 public struct Bitboard
 {
     public ulong bitboard;
--- MiniPGN/Board Representation/Pieces.cs
+++ MiniPGN/Board Representation/Pieces.cs
@@ -2,20 +2,22 @@ namespace MiniPGN.Board_Representation;
 
 public static class Pieces
 {
-    public const byte WPawn = 0b0000;
-    public const byte WKnight = 0b0001;
-    public const byte WBishop = 0b0010;
-    public const byte WRook = 0b0011;
-    public const byte WQueen = 0b0100;
-    public const byte WKing = 0b0101; // 5
+    public const byte WPawn = 0b0001;
+    public const byte WKnight = 0b0010;
+    public const byte WBishop = 0b0011;
+    public const byte WRook = 0b0100;
+    public const byte WQueen = 0b0101;
+    public const byte WKing = 0b0110; // 6
     
-    public const byte BPawn = 0b1000; // 8
-    public const byte BKnight = 0b1001;
-    public const byte BBishop = 0b1010;
-    public const byte BRook = 0b1011;
-    public const byte BQueen = 0b1100;
-    public const byte BKing = 0b1101; // 13
+    public const byte BPawn = 0b1001; // 9
+    public const byte BKnight = 0b1010;
+    public const byte BBishop = 0b1011;
+    public const byte BRook = 0b1100;
+    public const byte BQueen = 0b1101;
+    public const byte BKing = 0b1110; // 14
 
+    public const byte Empty = 0b0000;
+    
     public static int ColorOf(byte piece)
     {
         return piece >> 3;
