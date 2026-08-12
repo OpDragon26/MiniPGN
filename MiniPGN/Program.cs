d4e794de811569fad7e2c5df832ee2a0fb7b5a43
OpDragon26 <25opdragon25@gmail.com>
2026-08-12 23:43:40 +0200

    create project

---
+++ MiniPGN/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ChessLib.API.Display;
+using ChessLib.Base;
+
+Console.WriteLine(BoardDisplay.GetBoardString(Board.StartingBoard));
